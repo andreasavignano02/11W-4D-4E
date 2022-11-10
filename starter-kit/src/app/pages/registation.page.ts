d175d32571b8bbb9d6e6e4ff400583145ffdc0cf
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-11-10 16:04:32 +0100

    start

---
+++ starter-kit/src/app/pages/registation.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  template: `
+    <p>
+      registation works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class RegistationPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
